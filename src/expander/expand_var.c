d6fb3d5357a18bc57280f1d33f343b07032dd5df
Melanie Midon <mmidon@c3r2p3.42nice.fr>
2022-11-07 07:51:47 +0100

    norminette functions exept ft_expand_var which isn't finished

--- src/expander/expand_var.c
+++ src/expander/expand_var.c
@@ -8,10 +8,11 @@
 char	*ft_adjust(char *cmd, int i)
 {
 	char	*ret;
-	int	size;
+	int		size;
 
 	size = -1;
-	while (cmd[++size]);
+	while (cmd[++size])
+		;
 	ret = malloc(sizeof(char) * size + 1);
 	size = -1;
 	while (cmd[++size] && i--)
@@ -23,15 +24,16 @@ char	*ft_adjust(char *cmd, int i)
 
 char	*ft_env_chr(char *var, char **env)
 {
-	int	i;
+	int		i;
 	char	*ret;
-	int	var_len;
+	int		var_len;
 
 	var_len = ft_strlen(var);
 	i = -1;
 	while (env[++i])
 	{
-		if (!ft_strncmp(env[i], var + 1, var_len - 1) && env[i][var_len - 1] == '=')
+		if (!ft_strncmp(env[i], var + 1, var_len - 1)
+			&& env[i][var_len - 1] == '=')
 		{
 			ret = ft_strdup(env[i] + var_len);
 			return (ret);
@@ -45,12 +47,13 @@ char	*ft_find_var(char *cmd, int *i, int *j, char **env)
 	char	*var;
 	char	*ret;
 	char	*tmp;
-	int	k;
+	int		k;
 
 	tmp = NULL;
 	k = -1;
 	var = ft_calloc(sizeof(char), ARG_MAX);
-	while(cmd[*i + 1] && cmd[*i + 1] != 0 && cmd[*i + 1] != '$' && cmd[*i + 1] != ' ' && cmd[*i + 1] != '"')
+	while (cmd[*i + 1] && cmd[*i + 1] != 0 && cmd[*i + 1] != '$'
+		&& cmd[*i + 1] != ' ' && cmd[*i + 1] != '"')
 		var[++k] = cmd[(*i)++];
 	var[++k] = cmd[(*i)];
 	ret = ft_env_chr(var, env);
@@ -58,26 +61,26 @@ char	*ft_find_var(char *cmd, int *i, int *j, char **env)
 	{
 		free(var);
 		var = ft_find_var(cmd, i, j, env);
-	   if (ret)	
+		if (ret)
 			tmp = ft_strdup(ret);
 		free(ret);
 		ret = ft_strjoin(tmp, var);
 		free(tmp);
 	}
 	free(var);
-	return(ret);
+	return (ret);
 }
 
-char *ft_join(char *ret, char *var, int *j)
+char	*ft_join(char *ret, char *var, int *j)
 {
-	char *tmp;
-	char *tmp2;
+	char	*tmp;
+	char	*tmp2;
 
 	tmp = ft_strdup(ret);
 	tmp2 = ft_strjoin(tmp, var);
 	ft_strlcpy(ret, tmp2, ARG_MAX);
 	free(tmp2);
-	(*j)+= ft_strlen(var);
+	(*j) += ft_strlen(var);
 	free(var);
 	free(tmp);
 	return (ret);
@@ -85,8 +88,8 @@ char *ft_join(char *ret, char *var, int *j)
 
 char	*ft_expand_var(char *cmd, char **env)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
 	char	*var;
 	char	*ret;
 
@@ -97,7 +100,7 @@ char	*ft_expand_var(char *cmd, char **env)
 		return (NULL);
 	i = 0;
 	j = -1;
-	while(cmd[i])
+	while (cmd[i])
 	{
 		if (cmd[i] == '$')
 		{
@@ -113,8 +116,8 @@ char	*ft_expand_var(char *cmd, char **env)
 
 char	**ft_expand(char **cmd, t_ctx *ctx)
 {
-	int	i;
-	DIR *folder;
+	int		i;
+	DIR		*folder;
 	char	*cpy;
 //	struct dirent *entry;
 
@@ -136,9 +139,7 @@ char	**ft_expand(char **cmd, t_ctx *ctx)
 		}
 		if (ft_strchr(cmd[i], '"') || ft_strchr(cmd[i], '\''))
 		{
-			printf("before %s\n", cmd[i]); 
 			cpy = ft_no_random_quote(cmd[i]);
-			printf("after %s\n", cpy); 
 			free(cmd[i]);
 			cmd[i] = ft_strdup(cpy);
 			free(cpy);
--- src/expander/no_random_quote.c
+++ src/expander/no_random_quote.c
@@ -12,21 +12,21 @@ void	ft_count(int *counter, int *state, int quote_type)
 
 int	ft_malloc_without_quotes(char *cmd)
 {
-	int count_squotes;
-    int count_dquotes;
+	int	count_squotes;
+	int	count_dquotes;
 	int	state;
 	int	i;
 
 	state = 0;
 	count_dquotes = 0;
-    count_squotes = 0;
+	count_squotes = 0;
 	i = -1;
 	while (cmd[++i])
 	{
 		if (cmd[i] == '\'' && state != 2)
-        	ft_count(&count_squotes, &state, 1);
+			ft_count(&count_squotes, &state, 1);
 		else if (cmd[i] == '"' && state != 1)
-        	ft_count(&count_dquotes, &state, 2);
+			ft_count(&count_dquotes, &state, 2);
 	}
 	return (count_dquotes + count_squotes);
 }
@@ -42,20 +42,21 @@ void	ft_skip(int new_state, int *state)
 char	*ft_no_random_quote(char *cmd)
 {
 	char	*ret;
-	int state;
-	int	i;
-	int	j;
+	int		state;
+	int		i;
+	int		j;
 
 	state = 0;
 	j = -1;
 	i = 0;
-	ret = malloc(sizeof(char) * (ft_strlen(cmd) - ft_malloc_without_quotes(cmd) + 1));
+	ret = malloc(sizeof(char)
+			* (ft_strlen(cmd) - ft_malloc_without_quotes(cmd) + 1));
 	while (cmd[i])
 	{
 		if (cmd[i] == '\'' && state != 2)
-        	ft_skip(1 , &state);
+			ft_skip(1, &state);
 		else if (cmd[i] == '"' && state != 1)
-        	ft_skip(2 , &state);
+			ft_skip(2, &state);
 		else
 			ret[++j] = cmd[i];
 		if (cmd[i])
