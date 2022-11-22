402c0721bba0d1c77404268b8acdaa942c9f2cee
Melanie Midon <mmidon@c2r7p2.42nice.fr>
2022-11-22 09:04:24 +0100

    exit isn't segfaulting anymore

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: mmidon <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 12:24:21 by mmidon            #+#    #+#             */
-/*   Updated: 2022/11/21 12:25:02 by mmidon           ###   ########.fr       */
+/*   Updated: 2022/11/22 09:03:22 by mmidon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -20,6 +20,7 @@ int	built_exit(char **arg)
 {
 	int	i;
 
+	g_prompt.status = 0;
 	ft_putstr_fd("exit\n", 2);
 	i = -1;
 	while (arg[1] && arg[1][++i])
@@ -30,12 +31,13 @@ int	built_exit(char **arg)
 			exit(255);
 		}
 	}
-	if (arg[2])
+	if (arg[1] && arg[2])
 	{
 		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
 		return (1);
 	}
-	g_prompt.status = ft_atoi(arg[1]);
+	if (arg[1])
+		g_prompt.status = ft_atoi(arg[1]);
 	exit(g_prompt.status);
 	return (0);
 }
